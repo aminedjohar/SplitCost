1b19822b09ac87eb68618dd3f8d03059db617088
aminedjohar <aminedjohar@gmail.com>
2023-12-04 14:49:02 +0100

    update navbar to mobile friendly and add react-icons

--- src/components/landing-hero.jsx
+++ src/components/landing-hero.jsx
@@ -1,18 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Typed from 'react-typed';
 
-export default function LandingHero() {
-    return (
-        <div className='flex justify-center max-w-screen-xl mx-auto'>
-            <div className='p-40'>
-                <div className='text-6xl text-center font-bold '>Easy <span className='text-amber-300'>splitting</span><br /> of group <span className='text-red-600	'>expenses</span></div>
-                <div className='text-1xl text-center font-semibold mt-6'>The simplest way to calculate who owes what.</div>
-                <img
-                    src="/public/heroimg.png"
-                    className=''
-                    alt="Hero Image"
-                    // style="margin-top: -20px;"
-                />
-            </div>
-        </div>
-    )
-}
+const LandingHero = () => {
+  return (
+    <div className=''>
+      
+    </div>
+  );
+};
+
+export default LandingHero;
--- src/components/landing-navbar.jsx
+++ src/components/landing-navbar.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 export default function LandingNavbar() {
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
     return (
-        <div>
-            <header className="sticky top-0 z-[20] p-4">
-                <nav className="flex items-center justify-between max-w-screen-xl mx-auto">
-                    {/* Logo */}
-                    <div className="font-bold">
-                        <a href="/">/split<span className="text-red-600">cost</span></a>
-                    </div>
-
-                    {/* Nav Links */}
-                    <div className="flex space-x-6 font-semibold">
-                        <a href="#">FAQ</a>
-                        <a href="#">Privacy Policy</a>
-                        <a href="#">Example</a>
-                    </div>
-
-                    {/* Button CTA */}
-                    <button className="bg-red-500 text-white text-sm font-semibold py-2 px-4 rounded-full">
-                        START
-                    </button>
-                </nav>
-            </header>
+        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
+            <h1 className='w-full text-3xl font-bold text-[#000000]'>/Split<span className='text-[#00df9a]'>cost</span></h1>
+            <ul className='hidden md:flex'>
+                <li className='p-4'>Home</li>
+                <li className='p-4'>Company</li>
+                <li className='p-4'>Resources</li>
+                <li className='p-4'>About</li>
+                <li className='p-4'>Contact</li>
+            </ul>
+            <div onClick={handleNav} className='block md:hidden'>
+                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+            </div>
+            <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#ffffff] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+                <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+                <li className='p-4 border-b border-gray-600'>Home</li>
+                <li className='p-4 border-b border-gray-600'>Company</li>
+                <li className='p-4 border-b border-gray-600'>Resources</li>
+                <li className='p-4 border-b border-gray-600'>About</li>
+                <li className='p-4'>Contact</li>
+            </ul>
         </div>
-
-
-    )
-}
+    );
+};
